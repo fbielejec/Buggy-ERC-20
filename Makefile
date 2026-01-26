e7c8251867d5c25388b9e6f08dfbd76942aa240e
filip <fbielejec@gmail.com>
2026-01-26 14:52:35 +0100

    chall3

--- Makefile
+++ Makefile
@@ -9,3 +9,7 @@ test-challenge01:
 .PHONY: test-challenge02
 test-challenge02:
 	forge test --match-path test/Challenge02.t.sol -vv
+
+.PHONY: test-challenge03
+test-challenge03:
+	forge test --match-path test/Challenge03.t.sol -vv
--- test/Challenge03.t.sol
+++ test/Challenge03.t.sol
@@ -11,44 +11,21 @@ contract Challenge03Test is Test {
         token = new Challenge03();
     }
 
-    /* function test_RevertWhen_TransferFromInsufficientAllowance() public { */
-    /*   address owner = address(0xABCD); */
-    /*   token.mint(owner, 1e18); */
-
-    /*   address robber = address(0xBADD); */
-
-    /*   vm.prank(robber); */
-    /*   vm.expectRevert("insufficient allowance"); */
-    /*   token.transferFrom(owner, robber, 1e18); */
-    /* } */
-
     function test_RevertWhen_BurnInsufficientAllowance() public {
       address victim = address(0xBEEF);
-      
+
       // Test contract transfers tokens to victim
       token.transfer(victim, 1e18);
       assertEq(token.balanceOf(victim), 1e18);
 
       address blackhat = address(0xBADD);
-      
-      // Blackhat tries to burn victim's tokens (should succeed due to missing access control)
+
+      // The blackhat is not able to burn the victim's tokens
       vm.prank(blackhat);
+      vm.expectRevert("Challenge3: insufficient allowance");
       token.burn(victim, 1e18);
-      
-      // This assertion will fail because burn has no access control
-      // The blackhat was able to burn the victim's tokens
-      assertEq(token.balanceOf(victim), 0); // This is what actually happens
+
+      assertEq(token.balanceOf(victim), 1e18);
     }
-
-    /* function testFailBurnInsufficientBalance( */
-    /*     address to, */
-    /*     uint256 mintAmount, */
-    /*     uint256 burnAmount */
-    /* ) public { */
-    /*     burnAmount = bound(burnAmount, mintAmount + 1, type(uint256).max); */
-
-    /*     token.mint(to, mintAmount); */
-    /*     token.burn(to, burnAmount); */
-    /* } */
 
 }
